1eaf8987eecbc2c520f4fd9675171df7d96de66f
marlonsena_ctrl <marlondesena704@gmail.com>
2026-05-11 21:22:07 -0300

    Add files via upload

---
+++ new_sketch_1778537676640.ino
@@ -0,0 +1,33 @@
+/*
+ */
+
+int sensor=6;
+int led = 5;
+int motor = 4;
+
+bool chegouFim; 
+/* variável que guardará o estado lógico do botão:
+false (0): ainda não chegou no fim de curso
+ou true(1) chegou ao fim de curso */
+
+void setup() {
+  pinMode(sensor, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(motor, OUTPUT);
+  Serial.begin(9600); //inicia a comunicação pelo Monitor Serial
+}
+
+void loop() {
+
+  chegouFim = digitalRead(sensor);
+  
+  if(chegouFim){
+    Serial.println("tampa aberta");
+    digitalWrite(led, LOW);
+    digitalWrite(motor, LOW);  
+  }else{
+    Serial.println("tampa fechada");
+    digitalWrite(led, HIGH);
+    digitalWrite(motor, HIGH); 
+  } 
+}
